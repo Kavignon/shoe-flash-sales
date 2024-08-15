e2194c2040ef5fa208f660292d2646f3e8620d53
Kevin Avignon <kevin.avignon5@gmail.com>
2024-08-15 15:58:19 -0400

    #1 Added test suite for the Sale model.

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
+# Model class for representing the attributes and expected behavior for a Sale in a store.
 class Sale < ApplicationRecord
   belongs_to :inventory_item
 
   after_create :adjust_inventory
 
+  validates :quantity_sold, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
   private
 
-  def adjust_inventory(qty_item_sold: 0)
-    return unless qty_item_sold.present? && qty_item_sold.is_a?(Integer) && !qty_item_sold.negative?
+  def adjust_inventory
+    return unless quantity_sold.present? && quantity_sold.positive?
 
-    inventory_item.adjust_quantity!(quantity_sold: qty_item_sold)
+    inventory_item.adjust_quantity!(quantity_sold: quantity_sold)
   end
 end
--- test/models/sale_test.rb
+++ test/models/sale_test.rb
@@ -1,7 +1,38 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class SaleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @store = stores(:aldos_store)
+    @shoe = shoes(:running_shoe)
+    @inventory_item = inventory_items(:aldos_running_shoes)
+    @sale = @inventory_item.sales.build(quantity_sold: 5)
+  end
+
+  test 'should be valid with valid attributes' do
+    assert @sale.valid?
+  end
+
+  test 'should require a quantity_sold' do
+    @sale.quantity_sold = nil
+    assert_not @sale.valid?
+    assert_includes @sale.errors[:quantity_sold], "can't be blank"
+  end
+
+  test 'quantity_sold should be a positive integer' do
+    @sale.quantity_sold = -5
+    assert_not @sale.valid?
+    assert_includes @sale.errors[:quantity_sold], 'must be greater than 0'
+
+    @sale.quantity_sold = 0
+    assert_not @sale.valid?
+    assert_includes @sale.errors[:quantity_sold], 'must be greater than 0'
+  end
+
+  test 'adjust_inventory should adjust the inventory correctly' do
+    initial_quantity = @inventory_item.quantity
+    @sale.save
+    assert_equal initial_quantity - @sale.quantity_sold, @inventory_item.reload.quantity
+  end
 end
